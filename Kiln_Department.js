912d23e508e22c0f7dcc54bf259f569fd8286971
md.nurul <nurulislam8433@gmail.com>
2025-01-09 13:00:20 +0600

    'some'

---
+++ Kiln_Department.js
@@ -0,0 +1,25 @@
+
+
+const mouseover1 = document.querySelector('.tiles_picture1');
+const mouseover2 = document.querySelector('.tiles_picture2');
+
+const wall_tiles = document.querySelector('.wall_tiles');
+const out_put = document.querySelector('.out_put');
+
+const floor_tiles = document.querySelector('.floor_tiles');
+
+mouseover1.addEventListener('mouseover', function () {
+	console.log('rakib');
+	/*if(wall_tiles && out_put){
+		out_put.innerHTML = wall_tiles.innerHTML;
+    out_put.style.display = 'block'; 
+	out_put.style.color='red';
+	}*/
+    
+});
+
+mouseover1.addEventListener('mouseout', function () {
+   // out_put.style.display = 'none';
+});
+
+//console.log(mouseover2, mouseover1, wall_tiles, floor_tiles);
