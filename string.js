87be82679a8b12fb2f2211af732c187e0bb8f072
md.nurul <nurulislam8433@gmail.com>
2025-01-26 17:47:32 +0600

    'some'

--- string.js
+++ string.js
@@ -3,7 +3,7 @@
 2. concat (3 ways)
 */
 
-let roll=246718;
+/*let roll=246718;
 
 let numberTostring=roll.toString();
 
@@ -20,7 +20,7 @@ const seconConcat=A+B+C;
 const thirdConcat=`${A}${B}${C}`;
 
 console.log(firstConcat,seconConcat,thirdConcat);
-
+*/
 /*### string
 1. number to string
 2. concat (3 ways)
@@ -31,3 +31,26 @@ console.log(firstConcat,seconConcat,thirdConcat);
 7. find
 */
 
+let roll='246718';
+let string=roll.toString();
+console.log(string);
+
+//concat (3 ways); 0
+
+let a='apple';
+let b='bat';
+let c='cat';
+
+let firstconcat=a+b+c;
+let seconConcat=a.concat(b,c);
+let thiredConcat=`${a}${b}${c}`;
+
+console.log(firstconcat,seconConcat,thiredConcat);
+
+
+
+//string to array 
+
+let text="how are you doing today";
+ let array=text.split("");
+ console log(array);
